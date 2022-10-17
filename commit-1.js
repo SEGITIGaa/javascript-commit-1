0957dd3dbc12caedea7fe95b03bec7a2169af73e
SEGITIGaa <ilyasjuandaraya@gmail.com>
2022-10-17 18:52:54 +0700

    tugas 1

--- commit-1.js
+++ commit-1.js
@@ -1,1 +1,13 @@
-
+console.log('hello nigga!');
+console.log('-----------------');
+ 
+ // tambah
+ let nama='budi',prilaku=' jago sikap lilin';
+ console.log(nama + prilaku);
+ // kali
+ let x=20,y=10;
+ console.log(x*y);
+ // kurang
+ console.log(x-y);
+ // bagi
+ console.log(x/y);
